a2803b6cc22023d5b415073554644f816b5fcba7
Bailey K. Srimoungchanh <srimoungchanh.bailey@ku.edu>
2022-05-07 16:09:55 -0400

    Manual driving with DeepPicar

--- install/deeppicar_pkg/lib/python3.8/site-packages/deeppicar_pkg/deeppicar.py
+++ install/deeppicar_pkg/lib/python3.8/site-packages/deeppicar_pkg/deeppicar.py
@@ -1,29 +1,33 @@
 import os
 import json
 import rclpy
+from math import copysign
 from deeppicar_pkg import input_kbd
 from rclpy.node import Node
 
 from deepracer_interfaces_pkg.msg import (ServoCtrlMsg, CameraMsg)
 
-#Constants
-DEEPPICAR_DRIVE_TOPIC_NAME = "deeppicar_pkg/servo_msg"
+
 
 #Globals
 frame_id = 0
 
 class DeepPicarNode(Node):
+    #Constants
+    DEEPPICAR_DRIVE_TOPIC_NAME = "deeppicar_pkg/servo_msg"
+    CATEGORY_VALUES = [-0.85, -0.55, -0.35, 0.0, 0.35, 0.55, 0.85]
+
     #Local Variables
-    throttle = 0.0
+    throttle = 3
     use_dnn = False
-    angle = 0.0
+    angle = 3
     
     def __init__(self):
         super().__init__('deeppicar_node')
         self.get_logger().info("deeppicar_node started")
         # Taken from deepracer_navigation_pkg
         self.deeppicar_drive_publisher = self.create_publisher( ServoCtrlMsg, 
-                                                                DEEPPICAR_DRIVE_TOPIC_NAME,
+                                                                self.DEEPPICAR_DRIVE_TOPIC_NAME,
                                                                 1)
         # TODO: Add recorder callback function
         '''self.deeppicar_camera_subscriber = self.create_subscription( CameraMsg,
@@ -41,32 +45,22 @@ class DeepPicarNode(Node):
         servo = ServoCtrlMsg()
         if ch != 32:
             if ch == ord('j'): # left
-                self.angle += 0.1
-                self.angle = min(0.8, self.angle)
-                servo.angle = self.angle
+                self.angle = min(6, self.angle+1)
                 print ("\nleft")
             elif ch == ord('k'): # center
-                self.angle = 0.0
-                servo.angle = self.angle
+                self.angle = 3
                 print ("\ncenter")
             elif ch == ord('l'): # right
-                self.angle -= 0.1
-                self.angle = max(-0.8, self.angle)
-                servo.angle = self.angle
+                self.angle = max(0, self.angle-1)
                 print ("\nright")
             elif ch == ord('a'):
-                self.throttle -= 0.1
-                self.throttle = min(1.0, self.throttle)
-                servo.throttle = self.throttle
+                self.throttle = min(6, self.throttle+1)
                 print ("\naccel")
             elif ch == ord('s'):
-                self.throttle = 0.0
-                servo.throttle = 0.0
+                self.throttle = 3
                 print ("\nstop")
             elif ch == ord('z'):
-                self.throttle += 0.1
-                self.throttle = max(-1.0, self.throttle)
-                servo.throttle = self.throttle
+                self.throttle = max(0, self.throttle-1)
                 print ("\nreverse")
             elif ch == ord('r'):
                 change = False
@@ -79,8 +73,10 @@ class DeepPicarNode(Node):
             else:
                 change = False
             if change:
-                print('Throttle:' + str(self.throttle))
-                print('Angle:' + str(self.angle))
+                servo.throttle = self.CATEGORY_VALUES[self.throttle]
+                servo.angle = self.CATEGORY_VALUES[self.angle]
+                print('Throttle:' + str(servo.throttle))
+                print('Angle:' + str(servo.angle))
             self.deeppicar_drive_publisher.publish(servo)
 
     # TODO: Add recorder callback function
